3ebb1fdabcf2174eecd9e9bad7ff95b28eb97825
rocketvz <cloverdev88@gmail.com>
2021-09-01 07:35:33 -0400

    update the pool extend contract

--- contracts/CRPoolExtend.sol
+++ contracts/CRPoolExtend.sol
@@ -3,6 +3,7 @@ pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/proxy/Proxy.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/StorageSlot.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155Holder.sol";
 
 interface IBPools {
@@ -65,10 +66,21 @@ interface IBPools {
     ) external;
 }
 
-contract CRPoolExtend is Proxy, ERC1155Holder {
-    address public immutable implementation;
+contract CRPoolExtend is Proxy, ERC1155Holder  {
+    address public implementation;
     address public immutable exchangeProxy;
 
+    /**
+     * @dev Storage slot with the address of the current implementation.
+     * This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1, and is
+     * validated in the constructor.
+     */
+    bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+
+    /**
+     * @dev Emitted when the implementation is upgraded.
+     */
+    event Upgraded(address indexed implementation);
     constructor(
         address _poolImpl,
         address _exchProxy,
@@ -80,6 +92,22 @@ contract CRPoolExtend is Proxy, ERC1155Holder {
         if (_data.length > 0) {
             Address.functionDelegateCall(_poolImpl, _data);
         }
+    }
+
+    /**
+     * @dev Perform implementation upgrade
+     *
+     * Emits an {Upgraded} event.
+     */
+    function upgradeTo(address newImplementation) public {
+        // _setImplementation(newImplementation);
+        // emit Upgraded(newImplementation);
+        implementation = newImplementation;
+    }
+    
+    function _setImplementation(address newImplementation) private {
+        require(Address.isContract(newImplementation), "ERC1967: new implementation is not a contract");
+        StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value = newImplementation;
     }
 
     function _implementation() internal view override returns (address) {
