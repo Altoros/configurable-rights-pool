34a399f1d2c0972410aadea73545cc6b2f07ec29
rocketvz <cloverdev88@gmail.com>
2021-09-02 06:27:29 -0400

    update the crpextend

--- migrations/2_deploy_factories.js
+++ migrations/2_deploy_factories.js
@@ -8,43 +8,50 @@ const BFactory = artifacts.require('BFactory');
 const BalancerSafeMath = artifacts.require('BalancerSafeMath');
 const BalancerSafeMathMock = artifacts.require('BalancerSafeMathMock');
 const TToken = artifacts.require('TToken');
-const CRPool = artifacts.require('ConfigurableRightsPool')
+const SafeApprove = artifacts.require('SafeApprove');
+const CRPool = artifacts.require('ConfigurableRightsPool');
 module.exports = async function (deployer, network, accounts) {
     // if (network === 'development' || network === 'coverage') {
-    let impl = await deployer.deploy(BFactory);
-    let implode = await deployer.deploy(BalancerSafeMathMock);
-    const swapFee = Web3.utils.toWei('0.003');
-    const startWeights = [Web3.utils.toWei('12'), Web3.utils.toWei('1.5'), Web3.utils.toWei('1.5')];
-    const startBalances = [Web3.utils.toWei('80000'), Web3.utils.toWei('40'), Web3.utils.toWei('10000')];
-    let xyz = await TToken.new('XYZ', 'XYZ', 18);
-    let weth = await TToken.new('Wrapped Ether', 'WETH', 18);
-    let dai = await TToken.new('Dai Stablecoin', 'DAI', 18);
-    let WETH = weth.address;
-    let DAI = dai.address;
-    let XYZ = xyz.address;
-    const poolParams = {
-        poolTokenSymbol: "SYM",
-        poolTokenName: "NAME",
-        constituentTokens: [XYZ, WETH, DAI],
-        tokenBalances: startBalances,
-        tokenWeights: startWeights,
-        swapFee: swapFee,
-    }
+    // let impl = await deployer.deploy(BFactory);
+    // let implode = await deployer.deploy(BalancerSafeMathMock);
+    // const swapFee = Web3.utils.toWei('0.003');
+    // const startWeights = [Web3.utils.toWei('12'), Web3.utils.toWei('1.5'), Web3.utils.toWei('1.5')];
+    // const startBalances = [Web3.utils.toWei('80000'), Web3.utils.toWei('40'), Web3.utils.toWei('10000')];
+    // let xyz = await TToken.new('XYZ', 'XYZ', 18);
+    // let weth = await TToken.new('Wrapped Ether', 'WETH', 18);
+    // let dai = await TToken.new('Dai Stablecoin', 'DAI', 18);
+    // let WETH = weth.address;
+    // let DAI = dai.address;
+    // let XYZ = xyz.address;
+    // const poolParams = {
+    //     poolTokenSymbol: "SYM",
+    //     poolTokenName: "NAME",
+    //     constituentTokens: [XYZ, WETH, DAI],
+    //     tokenBalances: startBalances,
+    //     tokenWeights: startWeights,
+    //     swapFee: swapFee,
+    // }
 
+    console.log('accounts: ', accounts[0]);
     await deployer.deploy(BalancerSafeMath);
     await deployer.deploy(RightsManager);
     await deployer.deploy(SmartPoolManager);
+    await deployer.deploy(SafeApprove);
     deployer.link(BalancerSafeMath, CRPFactory);
     deployer.link(RightsManager, CRPFactory);
     deployer.link(SmartPoolManager, CRPFactory);
     deployer.link(BalancerSafeMath, CRPoolExtend);
     deployer.link(RightsManager, CRPoolExtend);
     deployer.link(SmartPoolManager, CRPoolExtend);
+    deployer.link(BalancerSafeMath, CRPool);
+    deployer.link(RightsManager, CRPool);
+    deployer.link(SafeApprove, CRPool);
+    deployer.link(SmartPoolManager, CRPool);
     var crpIml = await deployer.deploy(CRPool);
-    const crPoolExtend = await deployer.deploy(CRPoolExtend, [crpIml.address, accounts[0], ["0x6d6168616d000000000000000000000000000000000000000000000000000000", "0x4300000000000000000000000000000000000000000000000000000000000000", "0x53696c7665720000000000000000000000000000000000000000000000000000", "0x666972737420746f6b656e000000000000000000000000000000000000000000", "0x696d6167652075726c0000000000000000000000000000000000000000000000"]]);
-    console.log(crPoolExtend.address);
-    console.log(crPoolExtend.address)
-    await deployer.deploy(CRPFactory, [crPoolExtend.address]);
-
+    console.log('ConfigRightPool: ', crpIml.address);
+    const crPoolExtend = await deployer.deploy(CRPoolExtend, crpIml.address, accounts[0], []);
+    console.log('CRPoolExtend: ', crPoolExtend.address);
+    var crpFactory = await deployer.deploy(CRPFactory, crPoolExtend.address);
+    console.log('CRPFactory: ', crpFactory.address);
     // }
 };
--- truffle-config.js
+++ truffle-config.js
@@ -28,6 +28,9 @@ module.exports = {
     etherscan: {
         apiKey: process.env.ETHERSCAN_APIKEY
     },
+    api_keys: {
+        etherscan: process.env.ETHERSCAN_APIKEY
+    }, 
     networks: {
         development: {
             host: 'localhost', // Localhost (default: none)
@@ -49,6 +52,17 @@ module.exports = {
                 providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_APIKEY}`
             }),
             network_id: 3,          // Ropsten's id
+            // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+            // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+            // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+            // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+        },
+        rinkeby: {
+            provider: () => new HDWalletProvider({
+                privateKeys: [process.env.PRIVATE_KEY],
+                providerOrUrl: `https://rinkeby.infura.io/v3/${process.env.INFURA_APIKEY}`
+            }),
+            network_id: 4,          // Ropsten's id
             // gas: 5500000,        // Ropsten has a lower block limit than mainnet
             // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
             // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
